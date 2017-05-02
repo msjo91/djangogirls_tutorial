835803d708813d0ed652218cf25c4cb63c3f3ca2
msjo91 <msjo91@gmail.com>
2017-05-02 18:30:59 +0900

    Modify templates and static

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -118,7 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'ko-kr'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Seoul'
 
@@ -132,6 +132,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(ROOT_DIR, 'static_root')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIR = (STATIC_DIR,)
+STATICFILES_DIRS = (STATIC_DIR,)
